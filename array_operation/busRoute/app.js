e978a3b47dd32198b8c3deffc43038d1fe1ea724
mariusherlea <mariusherlea@gmail.com>
2023-09-19 11:35:49 +0300

    rough display result of bus routes

--- array_operation/busRoute/app.js
+++ array_operation/busRoute/app.js
@@ -4,16 +4,32 @@ let array = [
   { stop: "Sibot", ora: "7:20" },
   { stop: "Balomir", ora: "7:25" },
   { stop: "Tartaria", ora: "7:30" },
-  { stop: "VintDeJos", ora: "7:40" },
+  { stop: "VintuDeJos", ora: "7:40" },
   { stop: "AlbaIulia", ora: "7:50" },
 
   { stop: "AlbaIulia", ora: "8:00" },
-  { stop: "VintDeJos", ora: "8:10" },
+  { stop: "VintuDeJos", ora: "8:10" },
   { stop: "Tartaria", ora: "8:20" },
   { stop: "Balomir", ora: "8:25" },
   { stop: "Sibot", ora: "8:30" },
   { stop: "Vinerea", ora: "8:40" },
   { stop: "Cugir", ora: "8:50" },
+
+  { stop: "Cugir", ora: "9:00" },
+  { stop: "Vinerea", ora: "9:10" },
+  { stop: "Sibot", ora: "9:20" },
+  { stop: "Balomir", ora: "9:25" },
+  { stop: "Tartaria", ora: "9:30" },
+  { stop: "VintuDeJos", ora: "9:40" },
+  { stop: "AlbaIulia", ora: "9:50" },
+
+  { stop: "AlbaIulia", ora: "10:00" },
+  { stop: "VintuDeJos", ora: "10:10" },
+  { stop: "Tartaria", ora: "10:20" },
+  { stop: "Balomir", ora: "10:25" },
+  { stop: "Sibot", ora: "10:30" },
+  { stop: "Vinerea", ora: "10:40" },
+  { stop: "Cugir", ora: "10:50" },
 ];
 
 const perChunk = 7; // items per chunk
@@ -33,22 +49,26 @@ const result = array.reduce((resultArray, item, index) => {
 // console.log(result[1][1]);
 // console.log(result[0]);
 const establishSens = (result) => {
-  for (let i = 0; i < 2; i++) {
-    let indexStart = result[i].findIndex((Object) => {
-      return Object.stop === "Vint";
+  let x = 0;
+  var z = [];
+  while (x < result.length) {
+    let indexStart = result[x].findIndex((Object) => {
+      return Object.stop === "VintuDeJos";
     });
-    let indexFinish = result[i].findIndex((Object) => {
+    let indexFinish = result[x].findIndex((Object) => {
       return Object.stop === "Sibot";
     });
     if (indexStart < indexFinish) {
-      // console.log(indexStart, indexFinish, result[i]);
-      return result[i];
-    }
-  }
-};
+      console.log(indexStart, indexFinish, result[x]);
 
-document.getElementById("list").innerHTML = JSON.stringify(
-  establishSens(result)
-);
+      z.push(result[x]);
+    }
+    x++;
+  }
+  return z;
+};
+let z = establishSens(result);
+
+document.getElementById("list").innerHTML = JSON.stringify(z);
 // console.log(array[(0, 1)]);
 // console.log(array[(0, 0)]);
